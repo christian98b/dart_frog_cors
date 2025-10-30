f5de96081c13018b6e07b003682b911b842c35aa
Christian Bruns <christianbruns1039@gmail.com>
2025-10-30 14:33:51 +0100

    Refactor CORS middleware to support multiple allowed origins and update version to 2.1.0

--- lib/src/cors.dart
+++ lib/src/cors.dart
@@ -7,20 +7,27 @@ import 'package:dart_frog_cors/src/cors_defaults.dart';
 ///
 /// Also adds an immediate response to `OPTIONS` requests for preflight checks.
 Middleware cors({
-  String allowOrigin = CorsDefaults.allowOrigin,
+  List<String> allowedOrigins = const [CorsDefaults.allowOrigin],
   String allowMethods = CorsDefaults.allowMethods,
   String allowHeaders = CorsDefaults.allowHeaders,
   Map<String, String>? additional,
 }) {
-  final headers = {
-    HttpHeaders.accessControlAllowOriginHeader: allowOrigin,
-    HttpHeaders.accessControlAllowMethodsHeader: allowMethods,
-    HttpHeaders.accessControlAllowHeadersHeader: allowHeaders,
-    if (additional != null) ...additional,
-  };
-
   return (handler) {
     return (context) async {
+      String responseOrigin = allowedOrigins.first;
+
+      final requestOrigin = context.request.headers['origin'];
+      if (requestOrigin != null && allowedOrigins.contains(requestOrigin)) {
+        responseOrigin = requestOrigin;
+      }
+
+      final headers = {
+        HttpHeaders.accessControlAllowOriginHeader: responseOrigin,
+        HttpHeaders.accessControlAllowMethodsHeader: allowMethods,
+        HttpHeaders.accessControlAllowHeadersHeader: allowHeaders,
+        if (additional != null) ...additional,
+      };
+
       if (context.request.method == HttpMethod.options) {
         return Response(statusCode: HttpStatus.ok, headers: headers);
       }
